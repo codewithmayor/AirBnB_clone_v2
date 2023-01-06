b43fc266e33c8b787d70aa96a28cf0402c9acadf
JuskingA <juskingadjeinyarko@gmail.com>
2023-01-06 22:58:51 +0000

    update undo

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
-from os import getenv
-from models.base_model import BaseModel, Base
+""" State Module for HBNB project """
+import models
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = 'amenities'
+    """ Amenity: Class"""
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenities = relationship('Place',
-                                       secondary='place_amenity',
-                                       back_populates='amenities')
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
